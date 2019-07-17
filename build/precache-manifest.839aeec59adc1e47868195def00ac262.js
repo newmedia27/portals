49c38288434f9165a9c5cc14cab8d303ab35c81a
Sviatoslav <newmedia27@gmail.com>
2019-07-17 13:07:36 +0300

    add build

---
+++ build/precache-manifest.839aeec59adc1e47868195def00ac262.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dea4b97e70cf97bb9c23aae4ed3e8f4f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9e45da199f73ae74cc1e",
+    "url": "/static/css/main.f8b8217c.chunk.css"
+  },
+  {
+    "revision": "2ca676763a994825257c",
+    "url": "/static/js/2.6d4116ab.chunk.js"
+  },
+  {
+    "revision": "9e45da199f73ae74cc1e",
+    "url": "/static/js/main.71628aa6.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
